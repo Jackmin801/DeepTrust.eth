1e23e24a02b31211db565215143cd2036642cba1
Jackmin801 <ongjackm@gmail.com>
2024-10-20 05:46:45 +0000

    sample commit

--- llm_inference/deeptrust/models/llama/modeling_llama.py
+++ llm_inference/deeptrust/models/llama/modeling_llama.py
@@ -49,6 +49,8 @@ from transformers.utils import (
     logging,
     replace_return_docstrings,
 )
+
+from deeptrust.hashing import hash_tensor
 from .configuration_llama import LlamaConfig
 
 
@@ -1200,6 +1202,7 @@ class LlamaForCausalLM(LlamaPreTrainedModel, GenerationMixin):
         )
 
         hidden_states = outputs[0]
+        print(hash_tensor(hidden_states.detach()))
         if self.config.pretraining_tp > 1:
             lm_head_slices = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
             logits = [F.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
